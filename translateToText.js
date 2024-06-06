b74321ac8f6eb911d02c6880714692a7b5f462c2
Cristian-Zambrano <cristian.zambrano@epn.edu.ec>
2024-06-05 20:21:14 -0500

    Add_function: he implementado la función para diferenciar entre texto, numero, simbolo o matúscula que esté escrita en braille para traducirlo al español

--- translateToText.js
+++ translateToText.js
@@ -7,16 +7,16 @@ const brailleMap = {
     'z': '⠵', '1': '⠼⠁', '2': '⠼⠃', '3': '⠼⠉',
     '4': '⠼⠙', '5': '⠼⠑', '6': '⠼⠋', '7': '⠼⠛',
     '8': '⠼⠓', '9': '⠼⠊', '0': '⠼⠚',
-    'á': '⠷','é': '⠮', 'í': '⠌', 'ó': '⠬', 'ú': '⠾', 'ñ': '⠻','ü':'⠳',
+    'á': '⠷', 'é': '⠮', 'í': '⠌', 'ó': '⠬', 'ú': '⠾', 'ñ': '⠻', 'ü': '⠳',
     '.': '⠄', ',': '⠂', ';': '⠆', ':': '⠒', '_': '⠤',
     '¿': '⠢', '?': '⠢', '¡': '⠖', '!': '⠖', '"': '⠦',
-    '(': '⠣', ')': '⠜', '+': '⠐⠖', '*': '⠐⠦',  '=': '⠐⠶', '÷': '⠐⠌',  '-': '⠐⠤',
+    '(': '⠣', ')': '⠜', '+': '⠐⠖', '*': '⠐⠦', '=': '⠐⠶', '÷': '⠐⠌', '-': '⠐⠤',
     'A': '⠨⠁', 'B': '⠨⠃', 'C': '⠨⠉', 'D': '⠨⠙', 'E': '⠨⠑',
     'F': '⠨⠋', 'G': '⠨⠛', 'H': '⠨⠓', 'I': '⠨⠊', 'J': '⠨⠚',
     'K': '⠨⠅', 'L': '⠨⠇', 'M': '⠨⠍', 'N': '⠨⠝', 'O': '⠨⠕',
     'P': '⠨⠏', 'Q': '⠨⠟', 'R': '⠨⠗', 'S': '⠨⠎', 'T': '⠨⠞',
     'U': '⠨⠥', 'V': '⠨⠧', 'W': '⠨⠺', 'X': '⠨⠭', 'Y': '⠨⠽',
-    'Z': '⠨⠵',  ' ': ' '
+    'Z': '⠨⠵', ' ': ' '
 };
 
 const reverseBrailleMap = {
@@ -40,8 +40,36 @@ const reverseBrailleMap = {
     '⠨⠵': 'Z', ' ': ' '
 };
 
-function translateToText(text) { 
-    //TODO
+function translateToText(text) {
+    const brailleText = text;
+    let outputText = '';
+    let interrogationCount = 0;
+    let exclamationCount = 0;
+    for (let i = 0; i < brailleText.length; i++) {
+        const char = brailleText[i];
+        if (char === '⠼') {
+            i++;
+            outputText += reverseBrailleMap['⠼' + brailleText[i]];
+        } else if (char === '⠢') {
+            interrogationCount++;
+            outputText += (interrogationCount % 2 == 1) ? '¿' : '?';
+        } else if (char === '⠖') {
+            exclamationCount++;
+            outputText += (exclamationCount % 2 === 1) ? '¡' : '!';
+        } else if (char === '⠨') {
+            i++;
+            outputText += reverseBrailleMap['⠨' + brailleText[i]].toUpperCase();
+        } else if (char === '⠐') {
+            i++;
+            outputText += reverseBrailleMap['⠐' + brailleText[i]];
+        } else if (reverseBrailleMap[char]) {
+            outputText += reverseBrailleMap[char];
+        } else {
+            outputText += '❓';
+        }
+    }
+
+    return outputText;
 }
 
 module.exports = translateToText;
